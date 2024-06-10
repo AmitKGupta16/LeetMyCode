9725c16e861ef0a87c89175ea694f19c099c1697
AmitKGupta16 <113191889+AmitKGupta16@users.noreply.github.com>
2024-06-10 14:15:01 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (64.77%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -10,17 +10,36 @@ class Solution {
 
     // }
 
+    //  public int rob(int[] nums) {
+    //     int N = nums.length;
+    //     if(N==1) return nums[0];
+    //     int[] dp = new int[N];
+    //     dp[0] = nums[0];
+    //     dp[1] = Math.max(dp[0],nums[1]);
+    //     for(int i=2;i<N;i++) {
+    //         dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]);
+    //     }
+    //     return dp[N-1];
+        
+    // }
+
      public int rob(int[] nums) {
         int N = nums.length;
-        if(N==1) return nums[0];
-        int[] dp = new int[N];
-        dp[0] = nums[0];
-        dp[1] = Math.max(dp[0],nums[1]);
-        for(int i=2;i<N;i++) {
-            dp[i] = Math.max(dp[i-1], nums[i] + dp[i-2]);
-        }
-        return dp[N-1];
+       int[] dp = new int[N];
+       Arrays.fill(dp,-1);
+       return robbing(nums,N-1,dp);
         
+    }
+
+    public int robbing(int[] nums, int index, int[] dp) {
+        if(index < 0) return 0;
+        if(dp[index] != -1) return dp[index];
+        int nottake = robbing(nums,index-1,dp);
+        int take = nums[index] + robbing(nums,index-2,dp);
+        int ans = Math.max(take,nottake);
+        dp[index] = ans;
+        return ans;
+
     }
 
    
