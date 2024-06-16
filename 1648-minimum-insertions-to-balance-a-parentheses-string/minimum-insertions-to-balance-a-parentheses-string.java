597f61a335432260ddf0c596abc21b86de14260d
AmitKGupta16 <113191889+AmitKGupta16@users.noreply.github.com>
2024-06-16 16:03:24 +0530

    Time: 27 ms (30.49%) | Memory: 45.6 MB (18.86%) - LeetSync

--- 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
+++ 1648-minimum-insertions-to-balance-a-parentheses-string/minimum-insertions-to-balance-a-parentheses-string.java
@@ -1,29 +1,18 @@
 class Solution {
     public int minInsertions(String s) {
-        Stack<Character> stack=new Stack<>();
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            char c = s.charAt(i);
-            if(c == '('){
-                stack.push(c);
-            }
-            else{
-                if(i+1<s.length()&& s.charAt(i+1)==')'){
-                    i++;
-                }
-                else {
-                    count++;
-                }
-                
-                if(!stack.isEmpty()){
-                    stack.pop();
-                }
-                else{
-                    count++;
-                }
+               Stack<Character> stack = new Stack<>();
+        int count =0;
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i)=='(') stack.push('(');
+            else {
+                if(i+1 < s.length() && s.charAt(i+1)==')') i++;
+                else count++;
+                if(!stack.isEmpty()) stack.pop();
+                else count++;
             }
         }
-        count += 2*stack.size();
+        count += 2 * stack.size();
+
         return count;
     }
 }
