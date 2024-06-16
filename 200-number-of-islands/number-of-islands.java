3150970e0e16b15f9cdd2661c1d52e03f3302b72
AmitKGupta16 <113191889+AmitKGupta16@users.noreply.github.com>
2024-06-16 16:51:18 +0530

    Time: 2 ms (98.16%) | Memory: 49.2 MB (63.98%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -6,7 +6,7 @@ class Solution {
         boolean[][] visited = new boolean[N][M];
         for(int i=0;i<N;i++) {
             for(int j=0;j<M;j++) {
-                if(grid[i][j]=='1' && !visited[i][j]) {
+                if(grid[i][j]=='1') {
                     isValid(i,j,visited,grid);
                     count++;
 
@@ -18,10 +18,14 @@ class Solution {
     }
 
     public void isValid(int i, int j,boolean[][] visited, char[][] A) {
-        if(i<0 || j<0 || i >= A.length || j >= A[0].length || visited[i][j] || A[i][j]=='0'){
+        // if(i<0 || j<0 || i >= A.length || j >= A[0].length || visited[i][j] || A[i][j]=='0'){
+        //     return;
+        // }
+        if(i<0 || j<0 || i >= A.length || j >= A[0].length || A[i][j]=='0'){
             return;
         }
-        visited[i][j] = true;
+        A[i][j] = '0' ;
+       // visited[i][j] = true;
         isValid(i-1,j,visited,A);
         isValid(i+1,j,visited,A);
         isValid(i,j-1,visited,A);
